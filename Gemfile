69f5bfa4d2df566168fa79de9bb2e96abcfe3627
iainsmith <user@server.com>
2013-07-02 23:11:24 +0100

    removing datamapper

--- Gemfile
+++ Gemfile
@@ -1,13 +1,7 @@
 source 'https://rubygems.org'
-gem 'sinatra', '>= 1.0'
+gem 'sinatra', '>= 1.4.3'
 gem 'rake'
 gem 'data_mapper'
-gem 'dm-core'
-gem 'dm-sqlite-adapter'
-gem 'dm-timestamps'
-gem 'dm-validations'
-gem 'dm-aggregates'
-gem 'dm-migrations'
 gem 'haml'
 
 group :test do
--- environment.rb
+++ environment.rb
@@ -1,10 +1,5 @@
 require 'rubygems'
 require 'bundler/setup'
-require 'dm-core'
-require 'dm-timestamps'
-require 'dm-validations'
-require 'dm-aggregates'
-require 'dm-migrations'
 require 'haml'
 require 'ostruct'
 
@@ -21,5 +16,4 @@ configure do
   $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
   Dir.glob("#{File.dirname(__FILE__)}/lib/*.rb") { |lib| require File.basename(lib, '.*') }
 
-  DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{File.expand_path(File.dirname(__FILE__))}/#{Sinatra::Base.environment}.db"))
 end
